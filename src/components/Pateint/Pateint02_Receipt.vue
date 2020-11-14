86a19ba25919fda3f09c148bcbeb20f8452adc26
KBS10 <qjatnghldlxl@naver.com>
2020-11-14 13:40:31 +0900

    medical

---
+++ src/components/Pateint/Pateint02_Receipt.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="Pateint_Receipt">
+      <h2>외래 접수</h2>
+
+      <table class="Receipt_Table">
+        <tr>
+          <td>진료실</td>
+          <td>
+            <select id="clinic" name="select" style="width:200px; height:25px">
+              <option value="진료실1">진료실1</option>
+              <option value="진료실2">진료실2</option>
+              <option value="진료실3">진료실3</option>
+              <option value="진료실4">진료실4</option>
+              <option value="진료실5">진료실5</option>
+            </select>
+          </td>
+          <td>진료 과목</td>
+          <td>
+            <select id="medicalSubject" name="select" style="width:200px; height:25px">
+              <option value="정신과">정신과</option>
+              <option value="이비인후과">이비인후과</option>
+              <option value="정형외과">정형외과</option>
+              <option value="비뇨기과">비뇨기과</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td>초재 구분</td>
+          <td>
+            <select id="division" name="select" style="width:200px; height:25px">
+              <option value="초진">초진</option>
+              <option value="재진">재진</option>
+            </select>
+          </td>
+          <td>담당 의사</td>
+          <td>
+            <select id="doctor" name="select" style="width:200px; height:25px">
+              <option value="김범수">김범수</option>
+              <option value="변희주">변희주</option>
+              <option value="신천은">신천은</option>
+              <option value="이주용">이주용</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td>진료일자</td>
+          <td><input id="Date" type="Date" style="width:200px; height:25px"></td>
+          <td>수납 금액</td>
+          <td><div id="totalPrice" style="width: 200px; height: 25px"></div></td>
+        </tr>
+      </table>
+    <div id="Receipt_Btn">
+      <v-btn id="cancel">접수 취소</v-btn>
+      <v-btn id="modify">접수 수정</v-btn>
+      <v-btn id="receipt">접수</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#Patient_Receipt{
+  text-align: center;
+}
+.Receipt_Table td{
+  padding: 10px;
+}
+#clinic, #medicalSubject, #division, #doctor, #Date, #totalPrice{
+  border: 1px solid;
+}
+#Receipt_Btn{
+  float: right;
+  text-align: center;
+  padding: 10px;
+}
+#cancel, #modify, #receipt{
+  color: white;
+  background-color: skyblue;
+  margin: 0 5px;
+}
+</style>
---
+++ src/components/Pateint/Patient03_History.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="Patient_History">
+      <h2>진료 내역</h2>
+      <div id="history_Header">
+        <input id="history_Date" type="Date">
+         진료 여부
+
+        <div id="history_Choose">
+         <input type="radio" id="history_Radio_Total">
+         <label for="history_Radio_Total">전체</label>
+         <input type="radio" id="history_Radio_Complete">
+         <label for="history_Radio_Complete">완료</label>
+         <input type="radio" id="history_Radio_Wait">
+         <label for="history_Radio_Wait">대기</label>
+        </div>
+      </div>
+
+  <table class="history_Table">
+    <th>
+      <td>환자번호</td>
+      <td>성명</td>
+      <td>진료과목</td>
+      <td>진료실</td>
+      <td>담당의사</td>
+      <td>시간</td>
+      <td>진료현황</td>
+      <td>진료종료</td>
+    </th>
+
+    <tr>
+    </tr>
+  </table>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#Patient_History{
+  text-align: center;
+}
+#history_Header{
+  background-color: rgb(84, 181, 220);
+  color: white;
+}
+#history_Choose{
+  display :inline-block;
+  padding: 0px 10px;
+}
+.history_Table{
+  text-align: center;
+}
+.history_Table th{
+  background-color: rgb(84, 181, 220);
+  color: white;;
+}
+.history_Table td{
+  width: 100px;
+  border: 1px solid;
+}
+</style>

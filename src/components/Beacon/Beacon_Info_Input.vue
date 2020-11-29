119f7f227648c975bf2ecacb7ddea8b075ce406b
KBS10 <qjatnghldlxl@naver.com>
2020-11-29 21:15:20 +0900

    비콘 정보 DB 넘기기 페이지

--- src/components/Beacon/Beacon_Info_Input.vue
+++ src/components/Beacon/Beacon_Info_Input.vue
@@ -1,97 +1,134 @@
 <template>
-    <div id="Beacon_Info_Input">
-        <h1>비콘 정보 DB 넘기는 페이지</h1>
+  <div id="Beacon_Info_Input">
+    <h1>비콘 정보 DB 넘기는 페이지</h1>
 
-        <div class="BeaconRows">
-            <div class="BeaconRows-BeaconRowWrap" data-term-luid="beacon-1">
-                <span id="count">{{ count }}</span>
-                <div class="Input_Beacon_Info">
+    <div class="BeaconRows">
+      <div
+        v-for="beacon in this.$store.state.beacons"
+        :key="beacon"
+        class="BeaconRows-BeaconRowWrap"
+        data-term-luid="beacon-1"
+      >
+        <span id="count">{{ beacon.beacon_count }} </span>
+        <div class="Input_Beacon_Info">
+          <input type="text" id="room" v-model="beacon.room" hidden />
 
-                    <input type="text" id="room" v-model="room" hidden>
+          <label for="uuid">uuid</label>
+          <input type="text" id="uuid" v-model="beacon.uuid" />
 
-                    <label for="uuid">uuid</label> 
-                    <input type="text" id="uuid" v-model="uuid">
-                    
-                    <label for="Major">Major</label> 
-                    <input type="text" id="Major" v-model="major">
+          <label for="Major">Major</label>
+          <input type="text" id="Major" v-model="beacon.major" />
 
-                    <label for="Minor">Minor</label> 
-                    <input type="text" id="Minor" v-model="beacon_id_minor">
+          <label for="Minor">Minor</label>
+          <input type="text" id="Minor" v-model="beacon.beacon_id_minor" />
 
-                    <label for="lat">lat</label> 
-                    <input type="text" id="lat" v-model="lat">
+          <label for="lat">lat</label>
+          <input type="text" id="lat" v-model="beacon.lat" />
 
-                    <label for="lng">lng</label> 
-                    <input type="text" id="lng" v-model="lng">
-                </div>
-                <button id="plus">
-                    <i class="fas fa-plus" id="plus" ></i>
-                </button>
-                <button id="delete">
-                    <i class="fas fa-trash-alt" id="delete"></i>
-                </button>
-                <v-btn @click="axiosFunction()">저장</v-btn>
-            </div>
+          <label for="lng">lng</label>
+          <input type="text" id="lng" v-model="beacon.lng" />
         </div>
-
-    </div>    
+        <button id="plus">
+          <i class="fas fa-plus" id="plus" @click="addBeaconInput()"></i>
+        </button>
+        <button id="delete">
+          <i class="fas fa-trash-alt" id="delete"></i>
+        </button>
+      </div>
+      <v-btn @click="axiosFunction()">저장</v-btn>
+    </div>
+  </div>
 </template>
 <script>
 // import axios from 'axios'
 
-export default {
-    data()  {
-        return {
-            count : 1,
-            room                : this.$store.state.room,
-            uuid                : this.$store.state.uuid,
-            beacon_scanner_id   : this.$store.state.beacon_scanner_id,
-            major               : this.$store.state.major,
-            beacon_id_minor     : this.$store.state.beacon_id_minor,
-            lat                 : this.$store.state.lat,
-            lng                 : this.$store.state.lng,
-        }
-    },
-    mounted() {
-
-    },
-    methods: {
-        addBeaconInput(){
-            this.count++;
-            console.log(this.count)
-        },
-        axiosFunction(){
-            console.log("HI")
-            console.log(this.uuid)  
-            // var data = {
-            //         beacon_id_minor : this.beacon_id_minor,
-            //         uuid : this.uuid,
-            //         major : this.major,
-            //         lat : this.lat,
-            //         lng : this.lng,
-            //         beacon_scanner_id : this.beacon_scanner_id,
-            //         room : "dfasdf",
-            // } 
-            // const url = "http://192.168.0.8:8000/api/admin/beacon_create"
-            // axios.post(url, data)
-            // .then(function(response){
-            //     console.log(response);
-            // })
-            // .catch(function(error){
-            //     console.log(error)
-            // })
-
-        }
-    }
+let count = 1;
+class beaconInfo {
+  room;
+  uuid;
+  beacon_scanner_id;
+  major;
+  beacon_id_minor;
+  lat;
+  lng;
+  beacon_count = 1;
+  constructor(room, uuid, beacon_scanner_id, major, beacon_id_minor, lat, lng) {
+    this.room = room;
+    this.uuid = uuid;
+    this.beacon_scanner_id = beacon_scanner_id;
+    this.major = major;
+    this.beacon_id_minor = beacon_id_minor;
+    this.lat = lat;
+    this.lng = lng;
+    this.beacon_count = count++;
+  }
 }
+
+export default {
+  data() {
+    return {
+      room: '',
+      uuid: '',
+      beacon_scanner_id: '',
+      major: '',
+      beacon_id_minor: '',
+      lat: '',
+      lng: '',
+    };
+  },
+  mounted() {
+  },
+  methods: {
+    addBeaconInput() {
+      console.log("추가 버튼")
+      this.$store.state.beacons.push(
+        new beaconInfo(
+          this.room,
+          this.uuid,
+          this.beacon_scanner_id,
+          this.major,
+          this.beacon_id_minor,
+          this.lat,
+          this.lng,
+          this.count
+        )
+      );
+      console.log(this.$store.state.beacons);
+    },
+    deleteBeaconInput() {
+        console.log("삭제 버튼")
+    },
+    axiosFunction() {
+      console.log("axios 통신");
+      console.log(this.$store.state.beacons)
+      // var data = {
+      //         beacon_id_minor : this.beacon_id_minor,
+      //         uuid : this.uuid,
+      //         major : this.major,
+      //         lat : this.lat,
+      //         lng : this.lng,
+      //         beacon_scanner_id : this.beacon_scanner_id,
+      //         room : "dfasdf",
+      // }
+      // const url = "http://192.168.0.8:8000/api/admin/beacon_create"
+      // axios.post(url, data)
+      // .then(function(response){
+      //     console.log(response);
+      // })
+      // .catch(function(error){
+      //     console.log(error)
+      // })
+    },
+  },
+};
 </script>
 <style scoped>
-#count{
-    font-weight: bold;
-    font-size: 1.5em;
+#count {
+  font-weight: bold;
+  font-size: 1.5em;
 }
-.BeaconRows .BeaconRows-BeaconRowWrap .Input_Beacon_Info{
-    display: inline-block;
-    margin : 0 10px;
+.BeaconRows .BeaconRows-BeaconRowWrap .Input_Beacon_Info {
+  display: inline-block;
+  margin: 0 10px;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -8,30 +8,14 @@ export default new Vuex.Store({
     socketBeaconInfo  : [],
     markers : [],
     map : null,
-    beaconInfo: {
-      room : ' ',
-      uuid : '',
-      beacon_scanner_id : '',
-      major : '',
-      beacon_id_minor : '',
-      lat : '',
-      lng : '',
-    }
-
+    beacons : [{
+      count : 1,
+    }],
   },
   getters: {// computed
 
   },
   mutations: { 
-    setBeaconInfo(room, uuid, beacon_scanner_id, major, beacon_id_minor, lat, lng){
-      this.state.room                 = room;
-      this.state.uuid                 = uuid;
-      this.state.beacon_scanner_id    = beacon_scanner_id;
-      this.state.major                = major;
-      this.state.beacon_id_minor      = beacon_id_minor;
-      this.state.lat                  = lat;
-      this.state.lng                  = lng;
-    },
   },
   actions: {
   },
